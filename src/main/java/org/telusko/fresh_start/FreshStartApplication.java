775399a4af5ea0dfb6d6e6bd616b5d0a87f71813
RushithReddy39 <venkatrushithreddy@gmail.com>
2026-09-07 22:26:41 +0530

    ssssss

--- src/main/java/org/telusko/fresh_start/FreshStartApplication.java
+++ src/main/java/org/telusko/fresh_start/FreshStartApplication.java
@@ -9,9 +9,7 @@ public class FreshStartApplication {
     public static void main(String[] args) {
 
         SpringApplication.run(FreshStartApplication.class, args);
-        System.out.println("FreshStartApplication started");
-        System.out.println("Hello world");
-        System.out.println("FreshStartApplication started");
+
     }
 
 }
